fb3505f9c44cb1dec9f8288b2f71b31c3ea94b0a
pouya sadeqi <psadqi@gmail.com>
2025-03-28 17:34:39 +0330

    codes almost finished

--- Phase_2/client.py
+++ Phase_2/client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 #creating the server socket IPV4 (AF_INET) and TCP (SOCK_STREAM)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,34 +7,36 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #connecting to the server with specified ip address (your computer ip) and port number
 client_socket.connect((socket.gethostbyname(socket.gethostname()), 12345))
 
-
-def receive():
-    """receiving message from server"""
-
+def send_message():
+    """sending message to server"""
     while True:
+        message = input("")
+        client_socket.send(message.encode("utf-8"))
 
+
+def receive_message():
+    """receiving message from server"""
+    while True:
         #we need a try except so that our program doesn't crash
         try:
-
             # receiving information from the server (buffer size is 1024)
             message = client_socket.recv(1024).decode("utf-8")
-
             #name request
             if message == "what is your name? ":
-                print()
-                print("*" * 30)
                 name = input("please enter your name: ")
-                print("*"*30)
-                print()
                 client_socket.send(name.encode("utf-8"))
-
             else:
                 print(message)
-
         #if there was an error
         except:
             print("ERROR!")
             client_socket.close()
             break
 
-receive()
+#creating threads for sending and receiving messages continuously
+receive_thread = threading.Thread(target=receive_message)
+send_thread = threading.Thread(target=send_message)
+
+#starting the send and receiving functions
+receive_thread.start()
+send_thread.start()
--- Phase_2/server.py
+++ Phase_2/server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 #creating the server socket IPV4 (AF_INET) and TCP (SOCK_STREAM)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,18 +11,44 @@ server_socket.bind((socket.gethostbyname(socket.gethostname()), 12345))
 server_socket.listen()
 
 #a dictionary for clients
-clients = {}
+clients = dict()
 
-def noting():
-    print()
-    print("*" * 30)
-    print("server is looking for connection...")
-    print("*" * 30)
-    print()
+def show_message(message):
+    """show the messages for everyone"""
+    for client in clients.keys():
+        client.send(message)
 
-def connect():
+def receive_message(client_socket):
+    """receive message from client"""
+    while True:
+
+        # we need a try except so that our program doesn't crash
+        try:
+
+            #receive the message from the client
+            message = client_socket.recv(1024).decode("utf-8")
+            message = f"\033[1;34m\n\t{clients[client_socket]}: {message}\n\033[0m".encode("utf-8")
+
+            #show the message to everyone
+            show_message(message)
+
+        except:
+
+            name = clients[client_socket]
+
+            #remove the client from the dictionary
+            clients.pop(client_socket)
+
+            #close the connection for the client
+            client_socket.close()
+
+            #let others know that the client left the server
+            show_message(f"\033[1;31m\n\t{name} has left the server\n\033[0m".encode("utf-8"))
+            break
+
+
+def connect_client():
     """connecting to server"""
-
     while True:
 
         #accept incoming connections
@@ -33,13 +60,22 @@ def connect():
         client_name = client_socket.recv(1024).decode('utf-8')
 
         #adding the client to dictionary
-        clients[client_socket] = client_name
+        clients.update({client_socket: client_name})
         print(f"new client: {clients[client_socket]}")
 
         #informing the client
-        client_socket.send(f"welcome {clients[client_socket]}, you are connected to the server\n".encode('utf-8'))
+        client_socket.send(f"\nwelcome {clients[client_socket]}, you are connected to the server\n".encode('utf-8'))
+        show_message(f"{clients[client_socket]} has joined the server".encode("utf-8"))
+
+        #when a client connects to the server stat a thread
+        receive_thread = threading.Thread(target=receive_message, args=(client_socket,))
+        receive_thread.start()
 
 #set up the server
-noting()
-connect()
+print()
+print("*" * 30)
+print("server is looking for connection...")
+print("*" * 30)
+print()
+connect_client()
 

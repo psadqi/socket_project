1464dc5482bc29c5d464d9fd2b62ed0324bf8d1b
pouya sadeqi <psadqi@gmail.com>
2025-03-28 14:54:22 +0330

    .py files created

--- Phase_2/client.py
+++ Phase_2/client.py
@@ -1,1 +1,7 @@
 import socket
+
+#creating the server socket IPV4 (AF_INET) and TCP (SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connecting to the server with specified ip address (your computer ip) and port number
+client_socket.connect((socket.gethostbyname(socket.gethostname()), 12345))
--- Phase_2/server.py
+++ Phase_2/server.py
@@ -1,1 +1,20 @@
 import socket
+
+#creating the server socket IPV4 (AF_INET) and TCP (SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#bind the sever to your computer ip and a desired port
+server_socket.bind((socket.gethostbyname(socket.gethostname()), 12345))
+
+# put it on listen (for incoming connection)
+server_socket.listen()
+
+#a dictionary for clients
+clients = {}
+
+def receive():
+    pass
+
+
+def connect():
+    pass
